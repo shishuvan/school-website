2a786b18046dbaafa0b3b0b3877d5e4a75105689
rajshah854 <raj.shah854@gmail.com>
2016-04-27 09:57:31 +0530

    added pages and fixed make.py for folders

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # school-website
+
+# TODO
+
+1. Make all images at path `/images`
+2. Remove `<html>`, `<body>`, `<head>` tags
+3. Home page for each section
--- make.py
+++ make.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 files = os.listdir('src')
 
@@ -6,15 +7,32 @@ basefile = open('templates/base.html', 'r')
 basehtml = basefile.read()
 basefile.close()
 
-for filename in files:
-	sourcefile = open('src/' + filename, 'r')
-	content = sourcefile.read()
-	sourcefile.close()
+for basepath, folders, files in os.walk('src'):
+	relative_path = basepath[3:].strip(os.path.sep)
+	dist_path = os.path.join('dist', relative_path)
 
-	html = basehtml.format(content=content)
+	# make the folder if missing
+	if not os.path.exists(dist_path):
+		os.makedirs(dist_path)
+		print 'made {0}'.format(dist_path)
 
-	targetfile = open('dist/' + filename, 'w')
-	targetfile.write(html)
-	targetfile.close()
+	# make the html page
+	for filename in files:
+		if filename.endswith('.html'):
+			sourcefile = open(os.path.join(basepath, filename), 'r')
+			content = sourcefile.read()
+			sourcefile.close()
 
-	print filename
+			html = basehtml.format(content=content)
+
+			targetfile = open(os.path.join(dist_path, filename), 'w')
+			targetfile.write(html)
+			targetfile.close()
+
+			print 'updated {0}'.format(os.path.join(dist_path, filename))
+
+	# copy images
+	if relative_path=='images':
+		for filename in files:
+			shutil.copyfile(os.path.join(basepath, filename), os.path.join(dist_path, filename))
+			print 'copied {0}'.format(os.path.join(dist_path, filename))

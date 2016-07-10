104b8fcc34cbd57bf8734121fb9083b2bfdad3a6
Rushabh Mehta <rmehta@gmail.com>
2016-07-10 16:21:54 +0530

    added images

--- make.py
+++ make.py
@@ -1,38 +1,45 @@
 import os
 import shutil
+import time
 
-files = os.listdir('src')
+def make():
 
-basefile = open('templates/base.html', 'r')
-basehtml = basefile.read()
-basefile.close()
+	files = os.listdir('src')
 
-for basepath, folders, files in os.walk('src'):
-	relative_path = basepath[3:].strip(os.path.sep)
-	dist_path = os.path.join('dist', relative_path)
+	basefile = open('templates/base.html', 'r')
+	basehtml = basefile.read()
+	basefile.close()
 
-	# make the folder if missing
-	if not os.path.exists(dist_path):
-		os.makedirs(dist_path)
-		print 'made {0}'.format(dist_path)
+	for basepath, folders, files in os.walk('src'):
+		relative_path = basepath[3:].strip(os.path.sep)
+		dist_path = os.path.join('dist', relative_path)
 
-	# make the html page
-	for filename in files:
-		if filename.endswith('.html'):
-			sourcefile = open(os.path.join(basepath, filename), 'r')
-			content = sourcefile.read()
-			sourcefile.close()
+		# make the folder if missing
+		if not os.path.exists(dist_path):
+			os.makedirs(dist_path)
+			print 'made {0}'.format(dist_path)
 
-			html = basehtml.format(content=content)
-
-			targetfile = open(os.path.join(dist_path, filename), 'w')
-			targetfile.write(html)
-			targetfile.close()
-
-			print 'updated {0}'.format(os.path.join(dist_path, filename))
-
-	# copy images
-	if relative_path=='images':
+		# make the html page
 		for filename in files:
-			shutil.copyfile(os.path.join(basepath, filename), os.path.join(dist_path, filename))
-			print 'copied {0}'.format(os.path.join(dist_path, filename))
+			if filename.endswith('.html'):
+				sourcefile = open(os.path.join(basepath, filename), 'r')
+				content = sourcefile.read()
+				sourcefile.close()
+
+				html = basehtml.format(content=content)
+
+				targetfile = open(os.path.join(dist_path, filename), 'w')
+				targetfile.write(html)
+				targetfile.close()
+
+				print 'updated {0}'.format(os.path.join(dist_path, filename))
+
+		# copy images
+		if relative_path=='images':
+			for filename in files:
+				shutil.copyfile(os.path.join(basepath, filename), os.path.join(dist_path, filename))
+				print 'copied {0}'.format(os.path.join(dist_path, filename))
+
+while True:
+	make()
+	time.sleep(2)
